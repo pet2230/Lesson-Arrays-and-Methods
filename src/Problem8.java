d55957d0879a40299818c95fcc4c150de160f83c
pet2230 <peter.lu27@ycdsbk12.ca>
2025-12-03 14:20:49 -0500

    DiD stuff

--- src/Problem8.java
+++ src/Problem8.java
@@ -3,4 +3,8 @@ public class Problem8 extends ConsoleProgram {
     public void run() {
         for(int i = 0; i< arr.length; i++)
     }
+public int[] reverseCopy(int[] arr){
+
+        for(int i = 0; i< arr.length; i++)
+        }
 }
